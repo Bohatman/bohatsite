ea2408f4ab46f625d967fc13900f5c042a9ae235
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-03-12 09:11:23 +0700

    Update

--- phpworkshop/Chapter3.php
+++ phpworkshop/Chapter3.php
@@ -14,5 +14,12 @@
    printf ("This is PHP PRINTF<br>");
    printf ("6*5=%d",6*5);
    </script>
+   <!--This is HTML IN PHP TAGS-->
+   <?php 
+   <h1>ตัวอย่างที่ 1</h1>;
+   echo "<font color=red>";
+    echo "<br>HelloJa";
+    echo "</font>";
+   ?>
 </body>
 </html>
