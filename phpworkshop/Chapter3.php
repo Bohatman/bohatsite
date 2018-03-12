0bef0398ddce9915d7d40d033e5966aca72e66af
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-03-12 09:05:02 +0700

    UpdatepHp

--- phpworkshop/Chapter3.php
+++ phpworkshop/Chapter3.php
@@ -5,8 +5,12 @@
 </head>
 <body>
    <?php 
+   print "Hello World";
    echo "This is PHP World";
-   echo "1+2= ",1+2;
+   echo "<br>1+2= ",1+2;
    ?> 
+   <script language='php'>
+   print "GeeK House";
+   </script>
 </body>
 </html>
