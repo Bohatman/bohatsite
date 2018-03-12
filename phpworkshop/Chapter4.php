86907bd6b0429a2fa9a4854960a790a01b9a88e0
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-03-12 09:20:32 +0700

    Update

---
+++ phpworkshop/Chapter4.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Chapter 4 :เรื่องของตัวแปรและค่าคงที่</title>
+</head>
+<body>
+<?php 
+    $name = "Puttipong";
+    /// case sensitive
+    $Age = "20";
+    $age ="29";
+    echo "$name <br>";
+    echo "$Age <br>";
+    echo "$age <br>";
+    /// Use Variable as Integer
+    $num1 = 40+5;
+    $num2 = 30-8;
+    $result = $num1 - $num2;
+    echo "<font color = blue><b>result = </b></font>$result";
+?>
+</body>
+</html>
