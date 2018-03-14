d7c5e7ce8a44620af755aa35d3e76e3200d41a95
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-03-14 22:18:19 +0700

    Update Chapter

---
+++ phpworkshop/Chapter5_7.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Chapter 5 6 7 :ตัวดำเนินการและนิพจน์ใน PHP + เงื่อนไข + ฟังก์ชัน</title>
+</head>
+<body>
+<?php 
+$a = "Mark";
+$b = "a";
+$c = "Bohat";
+echo $a.$b.$c;
+?>
+</body>
+</html>
