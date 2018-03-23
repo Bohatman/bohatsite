d1c32af53b7ec6a6f7a8649b2fe48cef0194f39b
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-03-23 10:19:19 +0700

    Go

--- phpworkshop/Chapter9.php
+++ phpworkshop/Chapter9.php
@@ -5,11 +5,21 @@
 <body>
 <h1>FREE SPACE</h1>
 <hr/>
+<form action="" method="post">
+<button name = "Go">กดเพื่อเพิ่มข้อมูล</button></form>
 <?php 
-$handle1=fopen('text.txt','r');
-$content = fread($handle1, 50);
+$handle1=fopen("text.txt","r");
+$content = fpassthru($handle1);
 echo $content;
 fclose($handle1);
+if($_SERVER['REQUEST_METHOD']=="post" and isset($_POST['Go'])){
+    wF();
+}
+function wF(){
+    $handle2 = fopen("text.txt","w");
+    fputs($handle2, "<br> Add Add Add");
+
+}
 ?>
 </body>
 </html>
