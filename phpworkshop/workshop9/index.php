227d63211949161e02fefe21929b94486f565bb9
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-20 10:27:25 +0700

    f

--- phpworkshop/workshop9/index.php
+++ phpworkshop/workshop9/index.php
@@ -17,9 +17,9 @@
                                         if(!empty($_POST["txt_username"])&&!empty($_POST["txt_password"])){
                                                 $username = $_POST["txt_username"];
                                                 if($username== "admin"&& $_POST["txt_password"] == "enet"){
-                                                        
-                                                        echo "<div class=\"alert alert-danger col-sm-offset-2 col-sm-5 col-sm-5\">";
-                                                        echo "Username และ/หรือ Password ไม่ถูกต้อง</div>";
+                                                        echo "<div class=\"row\">"
+                                                        echo "<div class=\"alert alert-danger col-sm-offset-2\">";
+                                                        echo "Username และ/หรือ Password ไม่ถูกต้อง</div></div>";
                                                 
                                                 }
 
