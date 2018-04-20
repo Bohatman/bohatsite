aadaa6e6278126bf0fea6f117512fbb0760c6bac
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-20 09:40:19 +0700

    g

--- phpworkshop/workshop9/footer.php
+++ phpworkshop/workshop9/footer.php
@@ -1,4 +1,4 @@
 <?php 
-echo "<hr><br><small><font color=\"gray\" style=\"text-align: center;\">Create by Puttipong Suwannapornkul : 5903051623132</font></small><br><hr>";
+echo "<hr><center><br><small><font color=\"gray\">Create by Puttipong Suwannapornkul : 5903051623132</font></small><br></center><hr>";
 
 ?>
