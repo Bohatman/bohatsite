dfbbed5e7ffedc094b9444925f6aa0e748c1d48c
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-20 17:39:43 +0700

    ห

--- phpworkshop/workshop9/manage.php
+++ phpworkshop/workshop9/manage.php
@@ -61,7 +61,7 @@
                         </div>
                         <div class="form-group">
                         <div class="col-sm-offset-2 col-sm-10">
-                            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-download-alt"></span>บันทึก</button>
+                            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-download-alt"></span> บันทึก</button>
                         </div>
                         </div>
                     </form>
