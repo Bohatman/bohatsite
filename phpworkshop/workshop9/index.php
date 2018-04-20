4589c440d46db1b2dce3f963d37e794e2b688b5d
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-20 09:56:53 +0700

    fge

--- phpworkshop/workshop9/index.php
+++ phpworkshop/workshop9/index.php
@@ -28,7 +28,7 @@
                         </div>
                         <div class="form-group">
                                 <div class="col-sm-offset-2 col-sm-10">
-                                <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span>Submit</button>
+                                <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span> Login</button>
                         </div>
                         </div>
                         </form>
