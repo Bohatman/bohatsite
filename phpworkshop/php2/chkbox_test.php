e97669604786c91096cbd54e5dc881f93c7b16c5
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-20 08:56:48 +0700

    f

--- phpworkshop/php2/chkbox_test.php
+++ phpworkshop/php2/chkbox_test.php
@@ -1,9 +1,8 @@
 <?php 
 $n = count($_post["c"]);
 echo "Iam";
-echo $_POST["c"][0];
-for($i = 0 ; $i<$n;$i++){
-    echo $_post["c"][$i]."<BR>";
+for($i =0;$i<$n;$i++){
+    echo $_POST["c"][$i] . "<BR>";
 }
 
 ?>
