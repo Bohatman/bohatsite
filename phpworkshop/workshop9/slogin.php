d30865018f1d5de5c988098eb2e2810b80c37bfc
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-22 13:39:21 +0700

    asw

--- phpworkshop/workshop9/slogin.php
+++ phpworkshop/workshop9/slogin.php
@@ -1,12 +1,7 @@
-<script>
-    function logout(){
-            <?php 
-            session_unset();
-            header("Location: index.php");
-            ?>
-    }
 </script>
 <center>ออกจากระบบ
     <br>
-    <button type="submit" class="btn btn-default" onlick="">Logout</button>
+    <form action="logout.php" method="post">
+    <button type="submit" class="btn btn-default" onlick="logout();">Logout</button>
+</form>
 </center>
