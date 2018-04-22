762da3d6036a65d08c22b042499dfdd478674b9f
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-22 13:49:55 +0700

    asdw

--- phpworkshop/workshop9/logout.php
+++ phpworkshop/workshop9/logout.php
@@ -1,8 +1,9 @@
 <?php 
 $_SESSION["username"]=null;
+echo $_SESSION["username"];
 session_unset();
+echo $_SESSION["username"];
 session_destroy();
+echo $_SESSION["username"];
 ?>
-<script>
-    window.location.replace('index.php');
-</script>
+
