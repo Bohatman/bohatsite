9e596759e6a036dd4ecab9026aa9b826a19619a0
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-22 13:32:37 +0700

    asdqwdasd

--- phpworkshop/workshop9/slogin.php
+++ phpworkshop/workshop9/slogin.php
@@ -1,5 +1,11 @@
-
+<script>
+    function logout(){
+        <?php 
+            session_destroy();
+            ?>
+    }
+</script>
 <center>ออกจากระบบ
     <br>
-    <button type="submit" class="btn btn-default">Logout</button>
+    <button type="submit" class="btn btn-default" onlick="">Logout</button>
 </center>
