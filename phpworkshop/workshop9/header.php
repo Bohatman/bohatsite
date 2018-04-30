0981178744e26172aefbf8768e0902779eaed140
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-30 22:28:46 +0700

    de

--- phpworkshop/workshop9/header.php
+++ phpworkshop/workshop9/header.php
@@ -29,7 +29,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                             
-                            </button> <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-briefcase"></span> Workshop 6</a>
+                            </button> <a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-briefcase"></span> Workshop 6</a>
                             </div>
                             <div class="collapse navbar-collapse" id="collapse">
                             <ul class="nav navbar-nav">
