0082d138d9b211b4755951c34afabddcede2f76a
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-04-30 23:01:16 +0700

    s

--- phpworkshop/workshop9/header.php
+++ phpworkshop/workshop9/header.php
@@ -1,4 +1,11 @@
 <head>
+<head>
+                <meta charset="UTF-8" \>    
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                <link rel="shortcut icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Circle-icons-dev.svg/1024px-Circle-icons-dev.svg.png" /> 
 <script>
         function gourl(){
                 <?php 
