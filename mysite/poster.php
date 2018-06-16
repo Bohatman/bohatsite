8ab0d994512e8118ff5cfd52a4c0386ecbf57e9c
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-16 23:08:07 +0700

    asd

--- mysite/poster.php
+++ mysite/poster.php
@@ -30,7 +30,7 @@ if (!$link) {
 }
 
 $sql = "INSERT INTO short_post (DATE, DATA) 
-VALUES ('".$date."',\"".$comment."\")";
+VALUES ('".$date."',\"".$_POST['comment']."\")";
 
 if ($link->query($sql) === TRUE) {
     echo "New record created successfully";
