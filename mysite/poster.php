7de1165fcc7f5bc682c4838f5acf9ed0d5f2f38c
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-16 22:49:23 +0700

    s

--- mysite/poster.php
+++ mysite/poster.php
@@ -1,15 +1,39 @@
 <?php
-//if(!isset($_POST['username'] && !isset($_POST['comment']))){
-    //header("Location: index.html");
-//}
 $username = $_POST['username'];
 $comment =$_POST['comment'];
-$date = date("Y-m-d");
+$date = date("y-m-d");
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
-echo $date;
 
-//$sql = "INSERT INTO short_post (DATE, DATA) VALUES (".$date.",".$comment.")";
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+$sql = "INSERT INTO short_post (DATE, DATA) VALUES (".$date.",".$comment.")";
+
+if ($link->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $link->error;
+}
+
+mysqli_close($link);
 ?>
