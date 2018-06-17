fdec9d139e82a0664df2a6564a388335aef22ea1
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-17 17:37:39 +0700

    sda

--- mysite/post.php
+++ mysite/post.php
@@ -27,7 +27,7 @@ getdata($result,5);
 function getdata($result,$cout){
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc() 
-    if($row = null){ 
+    
     ?>
     <div class="media border p-3">
   <img src=".\pic\img_avatar3.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
@@ -45,7 +45,7 @@ if ($result->num_rows > 0) {
     </div>
     <?php
     
-    if($cout >=0){getdata($result,$cout-1);}}
+    if($cout >=0){getdata($result,$cout-1);}
         
   
 } else {
