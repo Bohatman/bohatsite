6179625a84144c55b6088a3b77b6e9b9a2e0f406
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-17 20:36:01 +0700

    sda

--- mysite/tester.php
+++ mysite/tester.php
@@ -10,7 +10,7 @@
 $(document).ready(function(){
     $('.button').click(function(){
         var clickBtnValue = $(this).val();
-        var ajaxurl = 'ajax.php',
+        var ajaxurl = 'some.php',
         data =  {'action': clickBtnValue};
         $.post(ajaxurl, data, function (response) {
             // Response div goes here.
