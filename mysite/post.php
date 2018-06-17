4ea9c61ab03ea9b2f3e5a722160776a5370d901b
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-18 04:38:45 +0700

    s

--- mysite/post.php
+++ mysite/post.php
@@ -4,7 +4,7 @@
   <title>Puttipong AKA. BOHAT</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="jquery-1.12.0.min.js" type="text/javascript"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
