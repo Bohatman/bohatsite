193ae382bee23ac88738a0fc617318a65d96c262
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-18 10:44:08 +0700

    s

--- mysite/post.php
+++ mysite/post.php
@@ -49,10 +49,10 @@ $(document).ready(function(){
     <div class="list_item">
     <div class="media border p-3">
     <div class="media-body">
+    <img src=".\pic\img_avatar3.png" alt="Avatar" class="ml-3 mt-3 rounded-circle" style="width:60px;">
     <h4><?php echo $username; ?><small><i> Posted on <?php echo $date; ?></i></small></h4>
     <p><?php echo $content; ?></p>
     </div>
-  <img src=".\pic\img_avatar3.png" alt="Avatar" class="ml-3 mt-3 rounded-circle" style="width:60px;">
 </div>
   </div>
     <?php } ?>
