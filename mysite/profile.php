face531f79d44cc2b864e724ad45597263c09060
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-18 17:18:04 +0700

    asd

--- mysite/profile.php
+++ mysite/profile.php
@@ -28,6 +28,7 @@
         </div>
         <div class="col-sm-9">
             <div class="border p-3">
+                <h1>ข้อมูลทั่วไป</h1><hr><br>
 <?php 
 require 'config.php';
     $query = $link->query("SELECT * FROM infomation");
