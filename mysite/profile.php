996efb14674c71347677373950e3cf746a0281c7
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-18 17:38:45 +0700

    asd

--- mysite/profile.php
+++ mysite/profile.php
@@ -27,7 +27,7 @@
 </div>
         </div>
         <div class="col-sm-9">
-            <div class="border p-3" style="margin-top:60px;">
+            <div class="border p-3">
                <br><h1>ข้อมูลทั่วไป</h1><hr><br>
 <?php 
 require 'config.php';
