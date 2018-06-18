866fd6e91c6b4548a21c0c686920786d23f9ec66
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-18 20:17:18 +0700

    se

--- mysite/profile.php
+++ mysite/profile.php
@@ -159,21 +159,21 @@ require 'config.php';
   </ul>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src=".\pic\azurebootcamp.jpg" alt="Los Angeles" width="1100" height="500">
+      <img src=".\pic\azurebootcamp.jpg" alt="Los Angeles" width="100%" height="500">
       <div class="carousel-caption">
         <h3>บริษัท ไมโครซอฟท์ (ประเทศไทย) จำกัด</h3>
         <p>กิจกรรมเกี่ยวกับการใช้งาน Microsoft Azure</p>
       </div>   
     </div>
     <div class="carousel-item">
-      <img src=".\pic\azuregekk.jpg" alt="Chicago" width="1100" height="500">
+      <img src=".\pic\azuregekk.jpg" alt="Chicago" width="100%" height="500">
       <div class="carousel-caption">
         <h3>Chulalongkorn University</h3>
         <p>การแข่งขัน Hackathon</p>
       </div>   
     </div>
     <div class="carousel-item">
-      <img src=".\pic\parallax.jpg" alt="New York" width="1100" height="500">
+      <img src=".\pic\parallax.jpg" alt="New York" width="100%" height="500">
       <div class="carousel-caption">
         <h3>King Mongkut's University of Technology North Bangkok</h3>
         <p>อบรมการสร้างเว็บไซต์แบบ Parallax</p>
