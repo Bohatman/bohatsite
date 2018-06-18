fa78e4a939435b0ff1a18b2b54c4972ac6996923
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-18 10:38:54 +0700

    asd

--- mysite/post.php
+++ mysite/post.php
@@ -34,29 +34,22 @@ $(document).ready(function(){
 });
 </script>
 <body>
-
-<?php 
-require 'config.php';
-$query = $link->query("SELECT * FROM short_post ORDER BY POSTID DESC LIMIT 2");
-  while($row = $query->fetch_assoc()){ 
-      $postID = $row['POSTID'];
-      $content = $row['DATA']; 
-      $date = $row['DATE'];
-      $username=$row['USERNAME'];
-      echo '<div class="list_item">';
-      echo '<div class="media border p-3">';
-      echo '<div class="media-body">';
-      echo '<h4>'.$username.'<small><i>Posted on '.$date.'</i></small></h4>';
-      echo '<p>'.$data.'</p>';
-      echo '</div>';
-      echo '<img src=".\pic\img_avatar3.png" alt="John Doe" class="ml-3 mt-3 rounded-circle" style="width:60px;">';
-      echo '</div>';
-      echo '<div class="show_more_main" id="show_more_main'.$postID.'">';
-      echo '<span id="'.$postID.'"class="show_more" title="Load more posts">Show more</span>';
-      echo '<span class="loding" style="display: none;"><span class="loding_txt">Loading...</span></span>';
-      echo '</div></div>';
-  }
-?>
-
+<div class="postList">
+    <?php
+    include 'config.php';
+    $query = $link->query("SELECT * FROM short_post ORDER BY POSTID DESC LIMIT 2");
+    
+    if($query->num_rows > 0){ 
+        while($row = $query->fetch_assoc()){ 
+            $postID = $row['POSTID'];
+    ?>
+    <div class="list_item"><?php echo $row['USERNAME']; ?></div>
+    <?php } ?>
+    <div class="show_more_main" id="show_more_main<?php echo $postID; ?>">
+        <span id="<?php echo $postID; ?>" class="show_more" title="Load more posts">Show more</span>
+        <span class="loding" style="display: none;"><span class="loding_txt">Loading...</span></span>
+    </div>
+    <?php } ?>
+</div>
 </body>
 </html>
