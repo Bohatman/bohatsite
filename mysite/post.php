dfd50ee3c7cd7a5c0db04e28633233af8c17d53e
Bohatman <32742355+Bohatman@users.noreply.github.com>
2018-06-18 10:56:34 +0700

    asd

--- mysite/post.php
+++ mysite/post.php
@@ -57,8 +57,8 @@ $(document).ready(function(){
   </div>
     <?php } ?>
     <div class="show_more_main" id="show_more_main<?php echo $postID; ?>">
-        <span id="<?php echo $postID; ?>" class="show_more" title="Load more posts">Show more</span>
-        <span class="loding" style="display: none;"><span class="loding_txt">Loading...</span></span>
+    <button type="button" class="btn btn-outline-primary"><span id="<?php echo $postID; ?>" class="show_more" title="Load more posts">Show more</span></button>
+    <button type="button" class="btn btn-outline-secondary"><span class="loding" style="display: none;"><span class="loding_txt">Loading...</span></span></button>
     </div>
     <?php } ?>
 </div>
